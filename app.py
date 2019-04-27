8e528815f914aba3e79659b3e517108510f97c63
Daniel Peach <dpeaches96@gmail.com>
2019-04-27 18:02:08 -0400

    fixed post index for deletion, using list comprehensions now

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from bottle import route, run, template, request, post, get, error, static_file,
 
 posts = [
     {
-        'id': 1,
+        'id': '1',
         'title': 'Post 1'
     },
     {
-        'id': 2,
+        'id': '2',
         'title': 'Post 2'
     }
 ]
@@ -21,54 +21,57 @@ def strip_trailing_slash():
 def index():
     return template('index.html')
 
-
+# Read (list)
 @get('/blog')
 def post_list():
     global posts
     return template('blog.html', posts=posts)
 
-
+# Read (detail)
 @get('/blog/<post_id>')
 def post_detail(post_id):
     global posts
-    post_id = int(post_id)
-    if post_id <= len(posts):
-        return template('post.html', post=posts[post_id - 1])
+    post_by_id = [post for post in posts if post['id'] == post_id]
+    if len(post_by_id) > 0:
+        return template('post.html', post=post_by_id[0])
     else:
         return abort(404, f'Post {post_id} cannot be found')
 
-
+# Create
+# Update
 @get('/blog/post-form')
 @get('/blog/post-form/<post_id>')
 def post_form(post_id=None):
     global posts
     if post_id:
-        post_id = int(post_id)
-        if post_id <= len(posts):
-            return template('post-form.html', post=posts[post_id - 1])
+        post_by_id = [post for post in posts if post['id'] == post_id]
+        if len(post_by_id) > 0:
+            return template('post-form.html', post=post_by_id[0])
         else:
             abort(404, f'Post {post_id} cannot be found')
     else:
         return template('post-form.html', post=None)
 
-
+# Create
+# Update
 @post('/blog/post-form')
 @post('/blog/post-form/<post_id>')
 def post_form_submit(post_id=None):
     global posts
     if post_id:
-        post_id = int(post_id)
-        if post_id <= len(posts):
+        post_by_id = [post for post in posts if post['id'] == post_id]
+        if len(post_by_id) > 0:
+            post_index = posts.index(post_by_id[0])
             updated_post = {
                 'id': post_id,
                 'title': request.forms.postTitle
             }
-            posts[post_id - 1] = updated_post
+            posts[post_index] = updated_post
         else:
             abort(404, f'Post {post_id} cannot be found')
     else:
         post_title = request.forms.postTitle
-        post_id = len(posts) + 1
+        post_id = str(len(posts) + 1)
 
         new_post = {
             'id': post_id,
@@ -79,13 +82,14 @@ def post_form_submit(post_id=None):
 
     return redirect(f'/blog/{post_id}')
 
-
+# Delete
 @post('/blog/post-delete/<post_id>')
 def post_delete(post_id):
     global posts
-    post_id = int(post_id)
-    if post_id <= len(posts):
-        del posts[post_id - 1]
+    post_by_id = [post for post in posts if post['id'] == post_id]
+    if len(post_by_id) > 0:
+        post_index = posts.index(post_by_id[0])
+        del posts[post_index]
     else:
         abort(404, f'Post {post_id} cannot be found')
     return redirect('/blog')
